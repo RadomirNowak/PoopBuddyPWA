8c77dffde989c735114ad5ea4a0415bcce26f75b
Radomir Nowak <kuker16@gmail.com>
2019-04-23 22:13:26 +0200

    [deployment] set up deployment of webapi and web to azure virtual machine

--- PoopBuddy/PoopBuddy.Web/Startup.cs
+++ PoopBuddy/PoopBuddy.Web/Startup.cs
@@ -25,7 +25,7 @@ namespace PoopBuddy.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsStaging())
             {
                 app.UseDeveloperExceptionPage();
             }
