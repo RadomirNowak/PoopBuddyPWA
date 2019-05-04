ff7785e606292af594a173dc0ef08994927bd230
Radomir Nowak <kuker16@gmail.com>
2019-05-04 02:02:44 +0200

    [pwa] reload the app if new version has been deployed

--- PoopBuddy/PoopBuddy.Web/ClientApp/src/app/modules/app/components/app.component.ts
+++ PoopBuddy/PoopBuddy.Web/ClientApp/src/app/modules/app/components/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {SwUpdate} from "@angular/service-worker"
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ClientApp';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        window.location.reload();
+      });
+    }
+  }
+
+  title = 'PoopBuddy';
+
+  constructor(private swUpdate: SwUpdate) {  }
 }
